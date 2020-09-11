ad450c74bfdd9a598a89b652196189fc43d88a91
Mazeorz <67795943+Mazeorz@users.noreply.github.com>
2020-09-11 16:18:26 +0800

    Update Duplicate-naming_EN.js

--- Sub-Store/Duplicate-naming_EN.js
+++ Sub-Store/Duplicate-naming_EN.js
@@ -2,7 +2,7 @@ function operator(proxies) {
     const counter = {};
     return proxies.map(p => {
       if (!counter[p.name]) counter[p.name] = 0;
-      p.name = p.name + " " + (++counter[p.name]+9).toString(36).toUpperCase();
+      p.name = p.name + " | " + (++counter[p.name]+9).toString(36).toUpperCase();
       return p;
     });
 }
