e0a1233385fb572354dafd403104f786f1d54e0c
Mazeorz <67795943+Mazeorz@users.noreply.github.com>
2020-09-12 16:23:06 +0800

    Update P2.js

--- Sub-Store/P2.js
+++ Sub-Store/P2.js
@@ -2,7 +2,7 @@ function operator(proxies) {
     const counter = {};
     return proxies.map(p => {
       if (!counter[p.name]) counter[p.name] = 0;
-      p.name = p.name + (++counter[p.name]).toString();
+      p.name = p.name + (("000" ++counter[p.name]).slice(-2)).toString();
       return p;
     });
 }
