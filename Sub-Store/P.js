2a8ad6dbe5b3be0da5a518f8ceafa3a8513f7673
Mazeorz <67795943+Mazeorz@users.noreply.github.com>
2020-09-12 12:04:19 +0800

    Update P.js

--- Sub-Store/P.js
+++ Sub-Store/P.js
@@ -1,11 +1,12 @@
 function operator(proxies) {
     const counter = {};
     return proxies.map(p => {
-      if (counter[p.name].count === 1){
-        return p
-      } eles{
-        if (!counter[p.name]) counter[p.name] = 0;
-      p.name = p.name + (++counter[p.name]).toString() + " | NF";
-      return p};
+        if (counter[p.name]){
+            counter[p.name] = 0; 
+            p.name = p.name;
+        } else {
+            p.name = p.name + (++counter[p.name]).toString() + " | NF";
+            }
+        return p;
     });
 }
